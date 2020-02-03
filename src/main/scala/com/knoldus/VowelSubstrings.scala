ad169fc1526c9610872afa741f0327050b238405
mansi7babbar <mansi7babbar@gmail.com>
2020-02-03 16:29:01 +0530

    updated source code of getting sum of number of vowels present in all possible substrings of a string

--- src/main/scala/com/knoldus/VowelSubstrings.scala
+++ src/main/scala/com/knoldus/VowelSubstrings.scala
@@ -1,23 +1,26 @@
+package com.knoldus
+
 object VowelSubstrings extends App {
-  def subString(str: String, n: Int): Int = {
+  def subString(str: String): Int = {
     val sum = new Array[Int](1)
 
-    def subStringRecursive(str: String, n: Int, sum: Array[Int], index: Int): Array[Int] = {
-      if (index > n - 1) {
+    @scala.annotation.tailrec
+    def subStringRecursive(str: String, sum: Array[Int], index: Int): Array[Int] = {
+      if (index > str.length - 1) {
         sum
       }
       else {
         if (str(index) == 'a' || str(index) == 'e' || str(index) == 'i' || str(index) == 'o' || str(index) == 'u') {
-          sum(0) = sum(0) + (index + 1) + ((index + 1) * (n - 1 - index))
+          sum(0) = sum(0) + (index + 1) + ((index + 1) * (str.length - 1 - index))
         }
-        subStringRecursive(str, n, sum, index + 1)
+        subStringRecursive(str, sum, index + 1)
       }
     }
 
-    subStringRecursive(str, n, sum, 0)
+    subStringRecursive(str, sum, 0)
     sum(0)
   }
 
   val str: String = scala.io.StdIn.readLine("Enter the string: ")
-  println("Sum of vowels present in all the possible substrings of the string: " + subString(str, str.length))
+  println("Sum of vowels present in all the possible substrings of the string: " + subString(str))
 }
